fdb23a7671d03cdbb4e49fed0f34d24330ef6ca4
Olivia <olivia.fintina@yahoo.com>
2024-03-25 21:16:11 +0200

    Materials for the fourth semester

---
+++ Semester 4/DBMS/Lab 1/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Lab_1_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.dataGridView1.Location = new System.Drawing.Point(90, 137);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(614, 312);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.dataGridView2.Location = new System.Drawing.Point(837, 137);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 8);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 82;
+            this.dataGridView2.RowTemplate.Height = 33;
+            this.dataGridView2.Size = new System.Drawing.Size(629, 312);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(352, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Locations";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1097, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NPCs";
+            // 
+            // updateButton
+            // 
+            this.updateButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.updateButton.Location = new System.Drawing.Point(597, 498);
+            this.updateButton.Margin = new System.Windows.Forms.Padding(4);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(128, 59);
+            this.updateButton.TabIndex = 4;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.deleteButton.Location = new System.Drawing.Point(809, 498);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(128, 59);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1574, 849);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ Semester 4/DBMS/Lab 1/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1_WF
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlDataAdapter dalocations;
+        SqlDataAdapter daNPCs;
+
+        DataSet dset;
+        BindingSource bslocations;
+        BindingSource bsNPCs;
+
+        SqlCommandBuilder cmdBuilder;
+
+        string locationsQuery;
+        string NPCsQuery;
+
+        string getConnectionString()
+        {
+            return "Data Source=DESKTOP-RJ1C8U7\\SQLEXPRESS;Initial Catalog=DnD;Integrated Security=True";
+        }
+
+        void FillData()
+        {
+            // Create a connection to the database
+            connection = new SqlConnection(getConnectionString());
+            locationsQuery = "SELECT * FROM locations";
+            NPCsQuery = "SELECT * FROM NPCs";
+
+            // Create data adapters for the locations and NPCs tables
+            dalocations = new SqlDataAdapter(locationsQuery, connection);
+            daNPCs = new SqlDataAdapter(NPCsQuery, connection);
+            dset = new DataSet();
+
+            // Fill the data set with the tables
+            dalocations.Fill(dset, "locations");
+            daNPCs.Fill(dset, "NPCs");
+
+            cmdBuilder = new SqlCommandBuilder(dalocations);
+
+            // Create a relation between the locations and NPCs tables
+            dset.Relations.Add("LocationInventory",
+                dset.Tables["Locations"].Columns["LocationID"], 
+                dset.Tables["NPCs"].Columns["LocationID"]);
+
+            //Populating the data grid views
+            this.dataGridView1.DataSource = dset.Tables["locations"];
+            //this.dataGridView2.DataSource = dset.Tables["NPCs"];
+            
+            this.dataGridView2.DataSource = this.dataGridView1.DataSource;
+            this.dataGridView2.DataMember = "LocationInventory";
+
+            cmdBuilder.GetUpdateCommand();
+        }
+
+        public Form1()
+        {
+            // Initialize the form
+            InitializeComponent();
+            FillData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Commit any pending edits in the DataGridView
+                dataGridView1.EndEdit();
+                dataGridView2.EndEdit();
+
+                // Update the data source
+                if (dset.HasChanges())
+                {
+                    // Ensure the connection is open
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+
+                    // Generate UpdateCommand for NPCs data adapter
+                    SqlCommandBuilder cmdBuilderNPCs = new SqlCommandBuilder(daNPCs);
+
+                    // Update both locations and NPCs tables
+                    dalocations.Update(dset, "locations");
+                    daNPCs.Update(dset, "NPCs");
+
+                    MessageBox.Show("Database updated successfully.");
+                }
+                else
+                {
+                    MessageBox.Show("No changes to update.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating database: " + ex.Message);
+            }
+            finally
+            {
+                // Close the connection
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView2.SelectedRows.Count > 0)
+                {
+                    int index = dataGridView2.SelectedRows[0].Index;
+                    DialogResult dr = MessageBox.Show("Are you sure you want to delete this NPC? This action cannot be undone.", "Confirm Deletion", MessageBoxButtons.YesNo);
+
+                    if (dr == DialogResult.Yes)
+                    {
+                        // Retrieve the NPC ID from the selected row in the child table
+                        int npcID = Convert.ToInt32(dataGridView2.Rows[index].Cells["NPCID"].Value);
+
+                        DataRow[] npcRows = dset.Tables["NPCs"].Select($"NPCID = {npcID}");
+                        if (npcRows.Length > 0)
+                        {
+                            npcRows[0].Delete();
+                        }
+
+                        // Configure the DeleteCommand for daNPCs
+                        daNPCs.DeleteCommand = new SqlCommand("DELETE FROM NPCs WHERE NPCID = @npcID", connection);
+                        daNPCs.DeleteCommand.Parameters.AddWithValue("@npcID", npcID);
+
+                        daNPCs.Update(dset, "NPCs");
+
+                        MessageBox.Show("NPC deleted successfully from database.");
+
+                        dataGridView2.Refresh();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select an NPC to delete.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting NPC: " + ex.Message);
+            }
+        }
+    }
+}
